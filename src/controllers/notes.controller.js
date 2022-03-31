42de9afe69c9df450abf68ad1065ef303c662502
mroldsma <81998465+mroldsma@users.noreply.github.com>
2022-03-31 11:41:44 -0300

    agr cosas

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -9,8 +9,9 @@ notesCtrl.renderNoteForm = (req,res) => {
 notesCtrl.createNewNote = async (req,res) =>{
   const {title, description} = req.body;
   const newNote = new Note({title, description});
+  newNote.user = req.user._id;
+  console.log(req.body)
   await newNote.save();
-  newNote.user = req.user.id;
   req.flash('success_msg', 'Note Added Successfully');
 
   res.redirect('/notes');
@@ -23,6 +24,10 @@ notesCtrl.renderNotes = async (req,res) =>{
 
 notesCtrl.renderEditForm = async (req,res) =>{
   const note = await Note.findById(req.params.id).lean();
+  if(note.user != req.user.id){
+    req.flash('error_msg', 'Not Authorized')
+    return res.redirect('/notes');
+  }
   res.render('notes/edit-note', { note });
 };
 
